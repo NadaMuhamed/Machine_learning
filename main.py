22f8e96bfc3c03891a07e9eadfb7aa38df362f2d
NadaMuhamed <77587501+NadaMuhamed@users.noreply.github.com>
2022-04-20 22:20:54 +0200

    push

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 from datetime import datetime
 from datetime import timedelta
 from sklearn.preprocessing import *
@@ -43,7 +44,7 @@ airline['price'] = Y
 ###########################"Model 1"###############################
 print("\n  Model 1  \n")
 corr = airline.corr()
-top_feature1 = corr.index[abs(corr['price'])>0.35]
+top_feature1 = corr.index[abs(corr['price'])>0.1]
 #Correlation plot
 plt.subplots(figsize=(12, 8))
 top_corr1 = airline[top_feature1].corr()
@@ -54,7 +55,7 @@ X = X[top_feature1]
 
 X_train1, X_test1, y_train1, y_test1 = train_test_split(X, Y, test_size = 0.50,shuffle=True,random_state=10)
 
-poly_features1 = PolynomialFeatures(degree=35)
+poly_features1 = PolynomialFeatures(degree=2)
 X_train_poly1 = poly_features1.fit_transform(X_train1)
 poly_model1 = linear_model.LinearRegression()
 poly_model1.fit(X_train_poly1, y_train1)
@@ -64,6 +65,7 @@ prediction1 = poly_model1.predict(poly_features1.fit_transform(X_test1))
 #print('Co-efficient of linear regression',poly_model1.coef_)
 #print('Intercept of linear regression model',poly_model1.intercept_)
 print('Mean Square Error', metrics.mean_squared_error(y_test1, prediction1))
+print('regression score function model 1', r2_score(y_test1, prediction1))
 true_price1=np.asarray(y_test1)[0]
 predicted_pruce1=prediction1[0]
 print('True price for the test set is : ' + str(true_price1))
@@ -71,7 +73,7 @@ print('Predicted price for the test set is : ' + str(predicted_pruce1))
 
 ###########################"Model 2"###############################
 print("\n  Model 2  \n")
-top_feature2 = corr.index[abs(corr['price'])>0.4]
+top_feature2 = corr.index[abs(corr['price'])>0.3]
 #Correlation plot
 plt.subplots(figsize=(12, 8))
 top_corr2 = airline[top_feature2].corr()
@@ -82,7 +84,7 @@ X = X[top_feature2]
 
 X_train2, X_test2, y_train2, y_test2 = train_test_split(X, Y, test_size = 0.30,shuffle=True,random_state=10)
 
-poly_features2 = PolynomialFeatures(degree=30)
+poly_features2 = PolynomialFeatures(degree=3)
 X_train_poly2 = poly_features2.fit_transform(X_train2)
 poly_model2 = linear_model.LinearRegression()
 poly_model2.fit(X_train_poly2, y_train2)
@@ -92,6 +94,8 @@ prediction2 = poly_model2.predict(poly_features2.fit_transform(X_test2))
 #print('Co-efficient of linear regression',poly_model2.coef_)
 #print('Intercept of linear regression model',poly_model2.intercept_)
 print('Mean Square Error', metrics.mean_squared_error(y_test2, prediction2))
+print('Mean Square Error', metrics.mean_squared_error(y_test2, prediction2))
+print('regression score function model 2', r2_score(y_test2, prediction2))
 true_price2=np.asarray(y_test2)[0]
 predicted_pruce2=prediction2[0]
 print('True price for the test set is : ' + str(true_price2))
@@ -99,7 +103,7 @@ print('Predicted price for the test set is : ' + str(predicted_pruce2))
 
 ###########################"Model 3"###############################
 print("\n  Model 3  \n")
-top_feature3 = corr.index[abs(corr['price'])>0.4]
+top_feature3 = corr.index[abs(corr['price'])>0.3]
 #Correlation plot
 plt.subplots(figsize=(12, 8))
 top_corr3 = airline[top_feature3].corr()
@@ -110,7 +114,7 @@ X = X[top_feature3]
 
 X_train3, X_test3, y_train3, y_test3 = train_test_split(X, Y, test_size = 0.40,shuffle=True,random_state=10)
 
-poly_features3 = PolynomialFeatures(degree=30)
+poly_features3 = PolynomialFeatures(degree=4)
 X_train_poly3 = poly_features3.fit_transform(X_train3)
 poly_model3 = linear_model.LinearRegression()
 poly_model3.fit(X_train_poly3, y_train3)
@@ -120,6 +124,7 @@ prediction3 = poly_model3.predict(poly_features3.fit_transform(X_test3))
 #print('Co-efficient of linear regression',poly_model3.coef_)
 #print('Intercept of linear regression model',poly_model3.intercept_)
 print('Mean Square Error', metrics.mean_squared_error(y_test3, prediction3))
+print('regression score function model 3', r2_score(y_test3, prediction3))
 true_price3=np.asarray(y_test3)[0]
 predicted_pruce3=prediction3[0]
 print('True price for the test set is : ' + str(true_price3))
@@ -127,6 +132,7 @@ print('Predicted price for the test set is : ' + str(predicted_pruce3))
 
 ###########################"Model 4"###############################
 print("\n  Model 4  \n")
+
 linear_model4 = linear_model.LinearRegression()
 linear_model4.fit(X,Y)
 prediction4= linear_model4.predict(X)
